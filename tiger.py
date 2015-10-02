fda588d76b16b72122e3c0c3d299577972f52db2
Samuel Tardieu <sam@rfc1149.net>
2015-10-02 15:18:58 +0200

    Add tiger.py executable

---
+++ tiger.py
@@ -0,0 +1,39 @@
+#! /usr/bin/python
+#
+# Do not modify
+
+from optparse import OptionParser
+from parser.parser import parse
+import sys
+
+parser = OptionParser()
+parser.add_option("-d", "--dump",
+                  help="dump input file to output",
+                  action="store_true", default=False,
+                  dest="dump")
+parser.add_option("-e", "--eval",
+                  help="evaluate input file to output",
+                  action="store_true", default=False,
+                  dest="eval")
+parser.usage = """%prog [options] [file]"""
+parser.description = "Compile a Tiger program (or standard input)"
+
+(options, args) = parser.parse_args()
+
+if len(args) > 1:
+    parser.print_help(file=sys.stderr)
+    sys.exit(1)
+
+fd = open(args[0]) if args else sys.stdin
+content = fd.read()
+fd.close()
+
+tree = parse(content)
+
+if options.dump:
+    from parser.dumper import Dumper
+    print(tree.accept(Dumper()))
+
+if options.eval:
+    from ast.evaluator import Evaluator
+    print("Evaluating: %s" % tree.accept(Evaluator()))
