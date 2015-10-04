e140511a9b2040a1a6875a5ab8fec665d987c2af
Samuel Tardieu <sam@rfc1149.net>
2015-10-04 09:13:09 +0200

    Add --expression

--- tiger.py
+++ tiger.py
@@ -11,6 +11,10 @@ parser.add_option("-d", "--dump",
                   help="dump input file to output",
                   action="store_true", default=False,
                   dest="dump")
+parser.add_option("-E", "--expression",
+                  help="use expression instead of file",
+                  action="store", default=None,
+                  dest="expression")
 parser.add_option("-e", "--eval",
                   help="evaluate input file to output",
                   action="store_true", default=False,
@@ -20,13 +24,16 @@ parser.description = "Compile a Tiger program (or standard input)"
 
 (options, args) = parser.parse_args()
 
-if len(args) > 1:
+if len(args) > 1 or (options.expression and len(args) > 0):
     parser.print_help(file=sys.stderr)
     sys.exit(1)
 
-fd = open(args[0]) if args else sys.stdin
-content = fd.read()
-fd.close()
+if options.expression:
+    content = options.expression
+else:
+    fd = open(args[0]) if args else sys.stdin
+    content = fd.read()
+    fd.close()
 
 tree = parse(content)
 
