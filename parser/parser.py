71290978ac3a9da959189c5a2a58bed74b76c5dc
Gabin Marignier <gabin.marignier@telecom-paristech.fr>
2015-10-23 19:08:57 +0200

    wip

--- parser/parser.py
+++ parser/parser.py
@@ -14,7 +14,7 @@ precedence = (
     ('right', 'UMINUS')
 )
 
-############## fonctionnel ##################
+############## Binary Operator ##################
 
 def p_expression_binop(p):
     '''expression : expression PLUS expression
@@ -29,38 +29,8 @@ def p_expression_binop(p):
                   | expression DIFFERENT expression
                   | expression OR expression
                   | expression AND expression'''
-    p[0] = BinaryOperator(p[2], p[1], p[3])
 
-def p_expression_parentheses(p):
-    'expression : LPAREN list RPAREN'
-    p[0] = SeqExp(p[2])
-
-def p_list(p):
-    '''list : 
-            | listexp'''
-    if len(p) == 1:
-        p[0] = []
-    else:
-        p[0] = p[1]
-
-def p_listexp(p):
-    '''listexp : expression
-               | listexp SEMICOLON expression'''
-    if len(p) == 2:
-        p[0] = [p[1]]
-    else:
-        p[0] = p[1] + [p[3]]
-
-def p_expression_ifthenelse(p):
-    '''expression : IF expression THEN expression ELSE expression'''
-    if len(p) == 7:
-        p[0] = IfThenElse(p[2], p[4], p[6])
-    else:
-        p[0] = IfThenElse(p[2], p[4], None)
-
-def p_expression_ifthen(p):
-    '''expression : IF expression THEN expression'''
-    p[0] = IfThenElse(p[2], p[4], None)
+############ Expressions #############
 
 def p_expression_number(p):
     'expression : NUMBER'
@@ -74,6 +44,37 @@ def p_expression_identifier(p):
     'expression : ID'
     p[0] = Identifier(p[1])
 
+   p[0] = BinaryOperator(p[2], p[1], p[3])
+
+########### Parenthesis ###############
+
+def p_expression_parentheses(p):
+    'expression : LPAREN list RPAREN'
+    p[0] = SeqExp(p[2])
+
+def p_list(p):
+    '''list : 
+            | listexp'''
+    if len(p) == 1:
+        p[0] = []
+    else:
+        p[0] = p[1]
+
+############ IfThenElse #############
+
+def p_expression_ifthenelse(p):
+    '''expression : IF expression THEN expression ELSE expression'''
+    if len(p) == 7:
+        p[0] = IfThenElse(p[2], p[4], p[6])
+    else:
+        p[0] = IfThenElse(p[2], p[4], None)
+
+def p_expression_ifthen(p):
+    '''expression : IF expression THEN expression'''
+    p[0] = IfThenElse(p[2], p[4], None)
+
+############ VarDecl ############
+
 def p_expression_vardecl(p):
     '''vardecl : VAR ID ASSIGN expression
                | VAR ID COLON INT ASSIGN expression'''
@@ -84,6 +85,8 @@ def p_expression_vardecl(p):
         p[0] = VarDecl(p[2], Type(p[4]), p[6])  
         #print('vardecl avec int')
 
+############ FunDecl #############
+
 def p_expression_fundecl(p):
     '''fundecl : FUNCTION ID LPAREN params RPAREN EQUAL expression
                | FUNCTION ID LPAREN params RPAREN COLON INT EQUAL expression'''
@@ -91,6 +94,23 @@ def p_expression_fundecl(p):
         p[0] = FunDecl(p[2], p[4], None , p[7])
     else:
         p[0] = FunDecl(p[2], p[4], Type(p[7]), p[9])
+
+def p_params(p):
+    '''params : 
+              | ID COLON INT
+              | params COMMA ID COLON INT'''
+    if len(p) == 1:
+        p[0] = []
+    elif len(p) == 4:
+        p[0] = [VarDecl(p[1], Type(p[3]), None)]
+        #print('params %s' % p[1])
+    elif len(p) == 6 and p[1] == []: 
+        raise Exception('Bad syntax in parser.py')
+    else:
+        p[0] = p[1] + [VarDecl(p[3], Type(p[5]), None)]
+        #print('params %s' % p[3])
+
+############ FunCall ##############
 
 def p_expression_funcall(p):
     '''expression : ID LPAREN explist RPAREN'''
@@ -109,20 +129,7 @@ def p_explist(p):
     else:
         p[0] = p[1] + [p[3]]
 
-def p_params(p):
-    '''params : 
-              | ID COLON INT
-              | params COMMA ID COLON INT'''
-    if len(p) == 1:
-        p[0] = []
-    elif len(p) == 4:
-        p[0] = [VarDecl(p[1], Type(p[3]), None)]
-        #print('params %s' % p[1])
-    elif len(p) == 6 and p[1] == []: 
-        raise Exception('Bad syntax in parser.py')
-    else:
-        p[0] = p[1] + [VarDecl(p[3], Type(p[5]), None)]
-        #print('params %s' % p[3])
+############## Let ################
 
 def p_expression_let(p):
     '''expression : LET decls IN listexp END
@@ -143,16 +150,26 @@ def p_decls(p):
     else:
         p[0] = p[1] + [p[2]]
 
-def p_error(p):
-    import sys
-    sys.stderr.write("no way to analyze %s\n" % p)
-    sys.exit(1)
+def p_listexp(p):
+    '''listexp : expression
+               | listexp SEMICOLON expression'''
+    if len(p) == 2:
+        p[0] = [p[1]]
+    else:
+        p[0] = p[1] + [p[3]]
+
+############## Assignment ##############
 
 def p_assignment(p):
     '''expression : ID ASSIGN expression'''
     p[0] = Assignment(Identifier(p[1]), p[3])
 
-############## test #################
+############## Parse error #############
+
+def p_error(p):
+    import sys
+    sys.stderr.write("no way to analyze %s\n" % p)
+    sys.exit(1)
 
 parser = yacc.yacc()
 
